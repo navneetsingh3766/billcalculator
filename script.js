a52386ca335d44f2672f549baff8907daee59248
navneet <navneet.singh.3766@gmail.com>
2020-08-18 15:32:35 +0530

    add numbers

--- script.js
+++ script.js
@@ -10,27 +10,33 @@ var finalPriceE = document.getElementById("finalPrice")
 var sel = document.getElementById("list");
 // check bos
 var checkBox = document.getElementById("checkBox");
+// number of picec
+var piece = document.getElementById("piece");
+// price without gst section
+var pricePera = document.getElementById("pricePera");
 
 //make enter price in round figure
 const roundedPrice = (priceStr)=>parseFloat(priceStr).toFixed(2);
 
+// get the pieces of models
+const getPiece = ()=> Number(piece.options[piece.selectedIndex].value);
 
-// function for adding 18% GST
+
+// function for adding 18% GST when price is without GST
 function calAddingGST18(enterRate) {
     let answer2 = roundedPrice(enterRate);
-    let a = (answer2 / 100 )*18 ;
-    let b = roundedPrice(a);
-    return b;
+    return ((answer2 / 100 )*18).toFixed(2);
 }
+
 // when price with gst
 function priceWithoutGST(price){
-    let ans = (price/118)*100;
+    let ans = ((price/118)*100);
     return ans;
 }
 // function that change prices
 function priceChange(enterRate) {
-    let raw = rawPriceE.innerHTML = roundedPrice(enterRate);
-    let gst = gstE.innerHTML = calAddingGST18(enterRate);
+    let raw = rawPriceE.innerHTML = roundedPrice(enterRate*getPiece());
+    let gst = gstE.innerHTML = calAddingGST18(enterRate*getPiece());
     finalPriceE.innerHTML = Number(raw)+Number(gst);
 }
 
@@ -38,9 +44,10 @@ function priceChange(enterRate) {
 function lossElement(basePrice, comparePrice){
     let li = ul.appendChild(document.createElement('li'));
     li.setAttribute("id", "loss");
-    li.appendChild(document.createTextNode("Loss:- "+ (basePrice-Number(comparePrice))));
+    li.appendChild(document.createTextNode("Loss:- "+ (basePrice-Number(comparePrice).toFixed(2))));
 
 }
+
 // condition checking for model
 function checkingSelectedModel(priceForCompare){
     if(sel.options[sel.selectedIndex].value==="1Lblack"){
@@ -70,10 +77,12 @@ addItemInput.addEventListener("keypress", function(event){
             if(document.getElementById("loss")){
                 document.getElementById("loss").remove();
             }
+            addMachineDetails();
             checkingSelectedModel(priceWithoutGST(roundedPrice(addItemInput.value)));
             addItemInput.value = "";
             console.log("checked" )
         } else if(!checkBox.checked){
+            addMachineDetails();
             priceChange(addItemInput.value)
         if(document.getElementById("loss")){
             document.getElementById("loss").remove();
@@ -92,10 +101,12 @@ submitButton.addEventListener("click", function(){
             if(document.getElementById("loss")){
                 document.getElementById("loss").remove();
             }
+            addMachineDetails();
             checkingSelectedModel(priceWithoutGST(roundedPrice(addItemInput.value)));
             addItemInput.value = "";
             console.log("checked" )
         } else if(!checkBox.checked){
+            addMachineDetails();
             priceChange(addItemInput.value)
         if(document.getElementById("loss")){
             document.getElementById("loss").remove();
@@ -107,3 +118,24 @@ submitButton.addEventListener("click", function(){
 })
 
 
+// making an element for machine details
+
+function addMachineDetails(){
+    let li = document.createElement('li');
+    let p = document.createElement('p');
+    p.setAttribute("id", "machineName");
+    p.setAttribute("class", "leftside");
+    p.appendChild(document.createTextNode(sel.options[sel.selectedIndex].text + " * ("+ getPiece()+") :-"));
+    li.appendChild(p);
+    let p2 = document.createElement('p');
+    p2.setAttribute("class", "rightside");
+    p2.setAttribute("id", "oneMachinePrice");
+    if(checkBox.checked){
+        p2.appendChild(document.createTextNode(roundedPrice(priceWithoutGST(addItemInput.value))));   
+    } else {
+        p2.appendChild(document.createTextNode(roundedPrice(addItemInput.value)));
+    }
+    li.appendChild(p2);
+    ul.insertAdjacentElement("beforeBegin",li);
+
+}
